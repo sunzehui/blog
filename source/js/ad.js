ed5227b5b835f12896442e0c0f9c4559303a4fa5
孙泽辉 <501574022@qq.com>
2024-04-01 15:30:48 +0800

    feat: 添加google ad

---
+++ source/js/ad.js
@@ -0,0 +1,5 @@
+let head = document.querySelector('head')
+let script = document.createElement('script')
+script.type = 'text/javascript'
+script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1234567890123456'
+head.appendChild(script)
