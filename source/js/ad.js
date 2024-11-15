9199b908436b4ffbdb5eb4db9ed6ee2a0bb8285e
孙泽辉 <501574022@qq.com>
2024-11-15 19:31:04 +0800

    update : add ads

--- source/js/ad.js
+++
@@ -1,6 +0,0 @@
-let head = document.querySelector('head')
-let script = document.createElement('script')
-script.type = 'text/javascript'
-script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9469686835170190'
-script.crossorigin = 'anonymous'
-head.appendChild(script)
